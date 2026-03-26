99a17bf0349982b4c2a3286d007e9e036e93cbd3
DioucleiRodrigues <dioucleirodriguesmoreira@gmail.com>
2026-03-26 20:41:11 -0300

    Add files via upload

---
+++ Lacorepeticaofor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lacorepeticaofor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lista = new List<string>();
+        lista.Add("Diouclei");
+        lista.Add("Rodrigues");
+        lista.Add("Moreira");
+
+        for (int i = 0; i < lista.Count; i++)
+        {
+            Console.WriteLine(lista[i]);
+        }
+    }
+}
