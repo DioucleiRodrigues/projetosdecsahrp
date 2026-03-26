3f2cac6c77326705137057e17ec4fbf109f7ec3d
DioucleiRodrigues <dioucleirodriguesmoreira@gmail.com>
2026-03-26 20:39:22 -0300

    Add files via upload

---
+++ Dicionarios/Program.cs
@@ -0,0 +1,28 @@
+﻿//Num dicionário passamos dois valores
+//Um para a chave e outro para o valor
+//O valor pode ser de qualquer tipo, inclusive outro dicionário
+using System;
+using System.Collections.Generic;
+
+namespace Dicionarios;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nome = new List<string>();
+        var dicionario = new Dictionary<int, string>();
+        dicionario.Add(1, "Diouclei");
+        dicionario.Add(2, "Giullia");
+        dicionario.Add(3, "Gustavo");
+        dicionario.Add(4, "Gusto");
+        nome.Add(dicionario[1]);
+        // Console.WriteLine(nome[0]);
+        //Console.WriteLine(dicionario[2]);
+
+        foreach (var item in dicionario)
+        {
+            Console.WriteLine($"Chave: {item.Key} - Valor: {item.Value}");
+        }
+    }
+}
