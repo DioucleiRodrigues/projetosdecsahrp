25bb8e032318a98c00504f99b45bd764c0d1d0c9
DioucleiRodrigues <dioucleirodriguesmoreira@gmail.com>
2026-03-26 20:42:23 -0300

    Add files via upload

---
+++ ArrayTipado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayTipado;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Random random = new Random();
+        var tipado = new int[6];
+        for (int i = 0; i < tipado.Length; i++)
+        {
+            tipado[i] = random.Next(1, 100);
+        }
+        Console.WriteLine("Valores do array:");
+        foreach (var value in tipado)
+        {
+            Console.WriteLine(value);
+        }
+
+        Array.Resize(ref tipado, 6);
+        Console.WriteLine("Valores do array após redimensionamento:");
+        */
+        //Listas 
+        var nomes = new List<string>();
+        nomes.Add("João");
+
+
+    }
+    //Caso eu preferisse aumentar o tamanho posso usar uma função de redimensionamento
+    // como Array.Resize(ref tipado, newSize);
+    //Com esta função posso criar um novo array com o tamanho desejado e copiar os valores do array original para o novo array, mantendo os valores existentes.
+
+
+
+
+
+
+}
+
+
---
+++ desafiobasico/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+namespace Desafiobasico;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var tentativas = 0;
+        Random numero = new Random();
+        var numeroSorteado = numero.Next(1, 100);
+        var acertou = false;
+
+        do
+        {
+            tentativas++;
+            Console.WriteLine("Digite um número entre 1 e 100: ");
+            var numeroDigitado = int.Parse(Console.ReadLine())!;
+            if (numeroDigitado == numeroSorteado)
+            {
+                Console.WriteLine($"Parabéns! Você acertou o número {numeroSorteado} em {tentativas} tentativas.");
+                acertou = true;
+                break;
+            }
+            else if (numeroDigitado < numeroSorteado)
+            {
+                Console.WriteLine("O número sorteado é maior. Tente novamente.");
+            }
+            else
+            {
+                Console.WriteLine("O número sorteado é menor. Tente novamente.");
+            }
+        }
+        while (tentativas < 4);
+        Console.WriteLine("");
+        if (!acertou)
+        {
+            Console.WriteLine($"Que pena! O número sorteado era {numeroSorteado}.");
+        }
+    }
+}
